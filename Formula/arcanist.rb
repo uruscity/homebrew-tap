4cf1a7019e56bc18e67ccc9d762fc799848fa9d1
Igor Shishkin <me@teran.ru>
2020-11-11 01:42:38 +0300

    Switch to upstream arcanist distribution
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- Formula/arcanist.rb
+++ Formula/arcanist.rb
@@ -5,23 +5,13 @@ class Arcanist < Formula
   homepage "https://secure.phabricator.com/book/phabricator/article/arcanist/"
 
   stable do
-    url "https://github.com/wikimedia/arcanist/archive/release/2019-08-22/1.tar.gz"
-    sha256 "6a5f726870224da8cf18d904bb96a1a6d8ee25d31c27ff8a0d2093884c243887"
-    version "201908221"
-
-    resource "libphutil" do
-      url "https://github.com/wikimedia/phabricator-libphutil/archive/release/2020-02-13/1.tar.gz"
-      sha256 "8a08c155f17a6a34c88ca23185e2b1506aa53a01a099e35fc6e9487c02340e4c"
-      version "202002131"
-    end
+    url "https://github.com/phacility/arcanist/archive/ac54d61d7af20f5d65ba889974f23a86bfb6cd57.tar.gz"
+    sha256 "d6edfccd857edf5e67df57a8019aa3c4cb9560dd48a93b1ddbe308fb546dc49c"
+    version "202010191"
   end
 
   head do
     url "https://github.com/phacility/arcanist.git"
-
-    resource "libphutil" do
-      url "https://github.com/phacility/libphutil.git"
-    end
   end
 
   depends_on 'php'
@@ -29,17 +19,9 @@ class Arcanist < Formula
   def install
     libexec.install Dir["*"]
 
-    resource("libphutil").stage do
-      (buildpath/"libphutil").install Dir["*"]
-    end
-
     prefix.install Dir["*"]
 
     bin.install_symlink libexec/"bin/arc" => "arc"
-
-    cp libexec/"resources/shell/bash-completion", libexec/"resources/shell/arc-completion.zsh"
-    bash_completion.install libexec/"resources/shell/bash-completion" => "arc"
-    zsh_completion.install libexec/"resources/shell/arc-completion.zsh" => "_arc"
   end
 
   test do
